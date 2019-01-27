ba939c7798eb9e2658dd8ddc7c212a8e118fca98
luoxiaole <luoxiaole@globalegrow.com>
2019-01-27 12:14:15 +0800

    first commit

--- site_list.py
+++ site_list.py
@@ -4,9 +4,11 @@ This module generates the listing of supported sites.
 """
 import json
 import sys
-import requests
 from argparse import ArgumentParser, RawDescriptionHelpFormatter
+
+import requests
 from bs4 import BeautifulSoup as bs
+
 
 def get_rank(domain_to_query):
     result = -1
@@ -14,7 +16,7 @@ def get_rank(domain_to_query):
     page = requests.get(url).text
     soup = bs(page, features="lxml")
     for span in soup.find_all('span'):
-    	if span.has_attr("class"):
+        if span.has_attr("class"):
             if "globleRank" in span["class"]:
                 for strong in span.find_all("strong"):
                     if strong.has_attr("class"):
@@ -22,10 +24,11 @@ def get_rank(domain_to_query):
                             result = int(strong.text.strip().replace(',', ''))
     return result
 
+
 parser = ArgumentParser(formatter_class=RawDescriptionHelpFormatter
                         )
-parser.add_argument("--rank","-r",
-                    action="store_true",  dest="rank", default=False,
+parser.add_argument("--rank", "-r",
+                    action="store_true", dest="rank", default=False,
                     help="Update all website ranks (not recommended)."
                     )
 args = parser.parse_args()
@@ -34,18 +37,18 @@ with open("data.json", "r", encoding="utf-8") as data_file:
     data = json.load(data_file)
 
 with open("sites.md", "w") as site_file:
-	data_length = len(data)
-	site_file.write(f'## List Of Supported Sites ({data_length} Sites In Total!)\n')
+    data_length = len(data)
+    site_file.write(f'## List Of Supported Sites ({data_length} Sites In Total!)\n')
 
-	index = 1
-	for social_network in data:
-		url_main = data.get(social_network).get("urlMain")
-		site_file.write(f'{index}. [{social_network}]({url_main})\n')
-		if args.rank == True:
-			data.get(social_network)["rank"] = get_rank(url_main)
-			sys.stdout.write("\r{0}".format(f"Updated {index} out of {data_length} entries"))
-			sys.stdout.flush()
-		index = index + 1
+    index = 1
+    for social_network in data:
+        url_main = data.get(social_network).get("urlMain")
+        site_file.write(f'{index}. [{social_network}]({url_main})\n')
+        if args.rank == True:
+            data.get(social_network)["rank"] = get_rank(url_main)
+            sys.stdout.write("\r{0}".format(f"Updated {index} out of {data_length} entries"))
+            sys.stdout.flush()
+        index = index + 1
 
 sorted_json_data = json.dumps(data, indent=2, sort_keys=True)
 
